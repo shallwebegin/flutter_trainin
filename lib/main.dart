d1d5c9c06092f48ef1328ae83dafdbca4526ee73
Berke Kartal <shallwebegin@users.noreply.github.com>
2024-02-26 01:50:08 +0300

    add tab_learn.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_trainin/101/navigation_learn.dart';
 import 'package:flutter_trainin/101/page_view_learn.dart';
 import 'package:flutter_trainin/101/statefull_life_cycle_learn.dart';
 import 'package:flutter_trainin/101/text_field_learn.dart';
+import 'package:flutter_trainin/202/tab_learn.dart';
 import 'package:flutter_trainin/demos/color_demos_view.dart';
 import 'package:flutter_trainin/demos/color_life_cycle_view.dart';
 import 'package:flutter_trainin/demos/my_collections_demos.dart';
@@ -23,6 +24,12 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
+        tabBarTheme: const TabBarTheme(
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.red,
+            indicatorSize: TabBarIndicatorSize.label),
+        bottomAppBarTheme:
+            const BottomAppBarTheme(shape: CircularNotchedRectangle()),
         progressIndicatorTheme:
             const ProgressIndicatorThemeData(color: Colors.white),
         appBarTheme: const AppBarTheme(
@@ -31,7 +38,7 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.transparent,
             elevation: 0),
       ),
-      home: NavigationLearn(),
+      home: TabLearn(),
     );
   }
 }
